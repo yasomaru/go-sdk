4f6510b9ae339e61d19d1d21a6495b55e08daa4a
Rob Findley <rfindley@google.com>
2025-05-13 20:58:47 +0000

    internal/mcp: merge the protocol package into mcp
    In accordance with the updated design, merge the protocol package into
    the mcp package. To do so, rename protocol.Content to WireContent and
    protocol.ResourceContents to WireResource.
    
    Also, remove some stray logging in readResource.
    
    Change-Id: I76f61ce8d67dd3ac520ef2e1959ade0a0785a480
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/672375
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -12,7 +12,6 @@ import (
 	"os"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
-	"github.com/modelcontextprotocol/go-sdk/mcp/protocol"
 )
 
 var httpAddr = flag.String("http", "", "if set, use SSE HTTP at this address, instead of stdin/stdout")
@@ -27,10 +26,10 @@ func SayHi(ctx context.Context, cc *mcp.ServerConnection, params *HiParams) ([]m
 	}, nil
 }
 
-func PromptHi(ctx context.Context, cc *mcp.ServerConnection, params *HiParams) (*protocol.GetPromptResult, error) {
-	return &protocol.GetPromptResult{
+func PromptHi(ctx context.Context, cc *mcp.ServerConnection, params *HiParams) (*mcp.GetPromptResult, error) {
+	return &mcp.GetPromptResult{
 		Description: "Code review prompt",
-		Messages: []protocol.PromptMessage{
+		Messages: []mcp.PromptMessage{
 			{Role: "user", Content: mcp.TextContent{Text: "Say hi to " + params.Name}.ToWire()},
 		},
 	}, nil
