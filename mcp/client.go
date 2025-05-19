eb76b5a6e009734cf657217bb77dbe105b5e11a3
Rob Findley <rfindley@google.com>
2025-05-19 20:52:51 +0000

    internal/mcp: remove misplaced TODO comment
    Remove a TODO that was in package doc position.
    
    Change-Id: Ib98dbcd7d733fdf0059bc3ced1fd6a1008b91a26
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/674257
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- mcp/client.go
+++ mcp/client.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-// TODO: consider passing Transport to NewClient and merging {Connection,Client}Options
 package mcp
 
 import (
